d0d971cd85e21a52e133b94d596527bc037e02a1
Lauro Cesar <lauro@hostcert.com.br>
2023-04-14 18:29:28 +0000

    House Keeping

--- upload_dev.sh
+++ upload_dev.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
 
-rsync -azv  --exclude=".python3/*" --exclude="bks/*" --exclude=".git/" --exclude=".venv/*" --exclude=".env/*"  --exclude=".cache/*"  --exclude="migrations/"  backend/ -e 'ssh' app@143.42.129.28:/home/app/console/
